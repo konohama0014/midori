1a17e698bacaa40f0a0abcbdbf919433acbac58e
konohama0014 <sereria@hotmail.com>
2022-08-18 03:41:37 +0900

    Slider

--- Assets/Script/zoomOut.cs
+++ Assets/Script/zoomOut.cs
@@ -5,19 +5,28 @@ using UnityEngine.UI;
 
 public class zoomOut : MonoBehaviour
 {
+    Slider zoomSlider;
+
     public GameObject mainCamObj;
 
-
+    float zoomPosi;
     // Start is called before the first frame update
     void Start()
     {
+        zoomSlider = GetComponent<Slider>();
+
+        float zoomMax = -10f;
+        float zoomMin = -100f;
+
+        //zoomSlider.maxValue = -10f;
+        //zoomSlider.value = -10f;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
@@ -29,6 +38,11 @@ public class zoomOut : MonoBehaviour
     public void touchZoomIn()
     {
         mainCamObj.transform.position = new Vector3(0, 0, -10);
+    }
+
+    public void zoomSlider_ontime()
+    {
+        mainCamObj.transform.position = new Vector3(0, 0, zoomSlider.value);
     }
 
 }
